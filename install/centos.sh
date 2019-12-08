4e89e77855a0227a71d43c19218eddd8f5974470
Danstan Onyango <danstan.otieno@gmail.com>
2019-12-09 00:21:31 +0300

    adds install centos

--- install/centos.sh
+++ install/centos.sh
@@ -9,25 +9,9 @@ sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/dock
 dnf list docker-ce --showduplicates | sort -r
 
 sudo dnf install -y docker-ce-3:18.09.1-3.el7
-
-sudo -i
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kube*
-EOF
-exit
  
-# Install Kubernetes and Docker
 sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes docker-ce docker-ce-cli containerd.io
  
-# Start Docker
 sudo systemctl enable --now docker
  
-# Start Kubernetes
 sudo systemctl enable --now kubelet
