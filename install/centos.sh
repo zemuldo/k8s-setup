fae49b458363b30665eafa73c9aba0cf33962144
Danstan Onyango <danstan.otieno@gmail.com>
2019-12-09 00:18:28 +0300

    adds install centos

--- install/centos.sh
+++ install/centos.sh
@@ -1,3 +1,7 @@
+
+# Turn off the swap: Required for Kubernetes to work
+sudo swapoff -a
+
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
@@ -6,6 +10,7 @@ dnf list docker-ce --showduplicates | sort -r
 
 sudo dnf install -y docker-ce-3:18.09.1-3.el7
 
+sudo -i
 sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -16,9 +21,7 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 exclude=kube*
 EOF
-
-# Turn off the swap: Required for Kubernetes to work
-sudo swapoff -a
+exit
  
 # Install Kubernetes and Docker
 sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes docker-ce docker-ce-cli containerd.io
