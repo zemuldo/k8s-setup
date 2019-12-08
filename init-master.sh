b7a9252f3a53afb708cc38b0b9eaa920d63c71f9
Danstan Onyango <danstan.otieno@gmail.com>
2019-12-08 22:54:08 +0300

    removes etcd

--- init-master.sh
+++ init-master.sh
@@ -1,6 +1,8 @@
 sudo kubeadm init --pod-network-cidr=192.168.122.0/24
 
-kubectl create -f etcd/manifest.yml
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl create -f calico/v3.10/manifest.yml
 
