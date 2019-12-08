f764c4d92183326251845751e4480ed96bacdf3a
Danstan Onyango <danstan.otieno@gmail.com>
2019-12-08 22:48:47 +0300

    patch

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -27,5 +27,3 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
 sudo apt install kubeadm kubelet kubectl
-
-sudo systemctl enable --now kubelet
