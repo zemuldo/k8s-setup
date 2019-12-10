413a4812f12071d54d70e2e770cda2efdaba60fb
Danstan Onyango <danstan.otieno@gmail.com>
2019-12-10 15:38:26 +0300

    adds storage os

---
+++ storageos/install.sh
@@ -0,0 +1,13 @@
+helm repo add storageos https://charts.storageos.com
+
+helm repo update
+
+helm install storageos/storageos --namespace storageos --generate-name --set cluster.join="z-server-2\,z-server-3\,z-server-4"
+
+ClusterIP=$(kubectl get svc/storageos --namespace storageos -o custom-columns=IP:spec.clusterIP --no-headers=true)\nApiAddress=$(echo -n "tcp://$ClusterIP:5705" | base64)\nkubectl patch secret/storageos-api --namespace storageos --patch "{\"data\": {\"apiAddress\": \"$ApiAddress\"}}"
+
+kubectl create -f storageos/storageclass.yml
+
+kubectl patch storageclass fast -p '{"metadata":{"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+kubectl --namespace storageos port-forward svc/storageos 5705
