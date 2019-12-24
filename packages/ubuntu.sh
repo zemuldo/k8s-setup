f824aefd282050ad7627fc082ed89c396427dc8e
Danstan Onyango <danstan.otieno@gmail.com>
2019-12-24 23:07:30 +0300

    update

--- packages/ubuntu.sh
+++ packages/ubuntu.sh
@@ -26,4 +26,6 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
+sudo apt-get update
+
 sudo apt install kubeadm kubelet kubectl -y
