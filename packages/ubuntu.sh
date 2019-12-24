c30e20cd0854daedb2711ece047dc659c2233f5c
Danstan Onyango <danstan.otieno@gmail.com>
2019-12-24 23:13:22 +0300

    update

--- packages/ubuntu.sh
+++ packages/ubuntu.sh
@@ -22,10 +22,16 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
 
 sudo apt-get update
 
-sudo apt install kubeadm kubelet kubectl -y
+sudo apt-get install -y kubelet kubeadm kubectl
+
+sudo apt-mark hold kubelet kubeadm kubectl
