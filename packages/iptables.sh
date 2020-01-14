9e5c92d0f0b072a24837bb32e73b1af46f10d93e
Danstan Onyango <danstan.otieno@gmail.com>
2020-01-14 20:06:54 +0300

    sudo

--- packages/iptables.sh
+++ packages/iptables.sh
@@ -1,7 +1,7 @@
-cat <<EOF >  /etc/sysctl.d/k8s.conf
+sudo cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-sysctl --system
+sudo sysctl --system
 
-swapoff -a
+sudo swapoff -a
