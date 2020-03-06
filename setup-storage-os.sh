e3b9369a7eab742bbb77de23b7ab978aab6198cf
Danstan Onyango <danstan.otieno@gmail.com>
2020-03-07 01:51:07 +0300

    update storageos

--- setup-storage-os.sh
+++ setup-storage-os.sh
@@ -1,4 +1,4 @@
-kubectl create -f https://github.com/storageos/cluster-operator/releases/download/1.5.1/storageos-operator.yaml
+kubectl create -f https://github.com/storageos/cluster-operator/releases/download/1.5.3/storageos-operator.yaml
 
 kubectl create -f storageos/storageos-operator-secret.yaml
 
