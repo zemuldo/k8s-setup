16d53e66216bfc0d0f15ed4f9d6f309926658e3e
Danstan Onyango <danstan.otieno@gmail.com>
2020-08-18 22:01:07 +0300

    Create amazon_linux.sh

---
+++ packages/amazon_linux.sh
@@ -0,0 +1,35 @@
+sudo swapoff -a
+
+sudo yum remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+sudo amazon-linux-extras install docker
+
+sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+ 
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+setenforce 0
+
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+systemctl enable firewalld
+
+sudo systemctl stop firewalld
